b45e2394d6de64299a3b6d7253a0e0bb45c41e8e
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-11-29 17:10:26 +0000

    Add wrappers for fashion-mnist

---
+++ slurm-wrappers/fashion-mnist-annoy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=fashion-mnist-annoy
+#SBATCH -o slurm_output/fashion-mnist-annoy.txt
+#SBATCH --workdir=/wrk/hyvi/ann-util
+#SBATCH -c 1
+#SBATCH -t 4:00:00
+#SBATCH --mem=2G
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ville.o.hyvonen@helsinki.fi
+
+module load Python/3.6.4-foss-2018a
+
+cd /wrk/hyvi
+source ann-env2/bin/activate
+
+cd ann-benchmarks
+srun python run.py --dataset fashion-mnist-784-euclidean --algorithm annoy --local --force --count 100
