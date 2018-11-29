0725a0a0c294901bd76e36818122ae24d8bab3cb
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-11-29 15:40:53 +0000

    Add hnswlib wrappers for sift and gist

---
+++ slurm-wrappers/gist-hnswlib.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=gist-hnswlib
+#SBATCH -o slurm_output/gist-hnswlib.txt
+#SBATCH --workdir=/wrk/hyvi/ann-util
+#SBATCH -c 1
+#SBATCH -t 20:00:00
+#SBATCH --mem=50G
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ville.o.hyvonen@helsinki.fi
+
+module load Python/3.6.4-foss-2018a
+
+cd /wrk/hyvi
+source ann-env2/bin/activate
+
+cd ann-benchmarks
+srun python run.py --dataset gist-960-euclidean --algorithm hnswlib --local --force --count 100
