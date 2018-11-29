9bc3eda663f8535118501210b81b57e05b55fa5e
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-11-29 15:31:44 +0000

    Add wrapper for hnswlib

---
+++ slurm-wrappers/mnist-hnswlib.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=mnist-hnswlib
+#SBATCH -o slurm_output/mnist-hnswlib.txt
+#SBATCH --workdir=/wrk/hyvi/ann-util
+#SBATCH -c 1
+#SBATCH -t 10:00:00
+#SBATCH --mem=10G
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ville.o.hyvonen@helsinki.fi
+
+module load Python/3.6.4-foss-2018a
+
+cd /wrk/hyvi
+source ann-env2/bin/activate
+
+cd ann-benchmarks
+srun python run.py --dataset mnist-784-euclidean --algorithm hnswlib --local --force --count 100
