51c404dd3504b472877ad1da0b3ec88eb5cfbe5d
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2018-12-02 18:41:16 +0000

    Update wrapper for nytimes-256-euclidean

--- slurm-wrappers/nytimes-256-euclidean-100.sh
+++ slurm-wrappers/nytimes-256-euclidean-100.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=nytimes-256-100
-#SBATCH -o slurm_output/nytimes-256-100.txt
+#SBATCH --job-name=nytimes-256-euclidean-100
+#SBATCH -o slurm_output/nytimes-256-euclidean-100.txt
 #SBATCH --workdir=/wrk/hyvi/ann-util
 #SBATCH -c 1
 #SBATCH -t 5:00:00
-#SBATCH --mem=10G
+#SBATCH --mem=100G
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ville.o.hyvonen@helsinki.fi
 
